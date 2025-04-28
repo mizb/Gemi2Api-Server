fce22425196c5b109ed36832432fd5904755ab1e
mizb <mi.zb@qq.com>
2025-04-28 09:27:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -363,4 +363,4 @@ async def root():
 if __name__ == "__main__":
 	import uvicorn
 
-	uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+	uvicorn.run("main:app", host="0.0.0.0", port=8700, log_level="info")
